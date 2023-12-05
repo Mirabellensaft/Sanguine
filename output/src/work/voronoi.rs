fc82a28663dd6fdd3ff0bde9d1e9c8d712ee28f5
Mirabellensaft <tanks@transfeld.net>
2023-12-05 18:08:34 +0100

    update readme

--- output/src/work/voronoi.rs
+++ output/src/work/voronoi.rs
@@ -1,20 +1,16 @@
-
-use sanguine_lib::resources::{layout, composition};
+use sanguine_lib::resources::{composition, layout};
 use svg::{node, Node};
 
 pub fn form_group(layout: &layout::Grid) -> node::element::Group {
-// pub fn form_group() {
+    // pub fn form_group() {
     let mut graph = node::element::Group::new();
 
     let voi_diagram = composition::voronoi::VoronoiDiagram::new_uniform(layout, 100);
 
     for cell in voi_diagram.cells {
         for line in cell.0 {
-            graph.append(line.draw()); 
-
+            graph.append(line.draw());
         }
-
     }
     graph
 }
-
